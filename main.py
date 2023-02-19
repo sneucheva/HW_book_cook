334697bf58b4bb0d54eb810f9c6eef7be0d13ad8
sneucheva <121288212+sneucheva@users.noreply.github.com>
2023-02-19 16:27:05 +0300

    First task

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-with open ('recipes.txt', encoding='utf-8') as book_cook:
-    res = book_cook.read()
-    print(res)
+with open ('recipes.txt', encoding='utf-8') as f:
+    book_cook = {}
+    for line in f:
+        bk_dish = line
+        bk_count = int(f.readline())
+        bk_product = []
+        for i in range(bk_count):
+            bkp = f.readline().strip()
+            ingredient, quantity, gauge = bkp.split(' | ')
+            bk_product.append({
+                'ingredient': ingredient,
+                'quantity': quantity,
+                'gauge': gauge
+            })
+        f.readline()
+        book_cook[bk_dish] = bk_product
+for x in book_cook:
+    print(x)
+    for y in book_cook[x]:
+        print(y)
+
+
+
+#def get_shop_list_by_dishes(dishes, person_count):
+ #   dishes = input('Введите названия блюд из заказа: ')
+  #  person_count = int(input('Введите количество персон: '))
+
+   # return(get_shop_list_by_dishes(dishes, person_count))
+#print(get_shop_list_by_dishes(dishes, person_count))
+
+
